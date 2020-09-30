398a7d838618745de52c644582ea249dc27f84a8
Vyacheslav <slavasan05@gmail.com>
2020-09-30 23:15:05 +0200

    add build

---
+++ build/precache-manifest.f98cdace464100beb0db6bc5939563b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c89035c1b1b3a2f8a6bd1653c09643a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c45a46c0a831078173c",
+    "url": "/static/js/2.48b3e9ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.48b3e9ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481307ed014c7e98f3a7",
+    "url": "/static/js/main.9b1f5ba8.chunk.js"
+  },
+  {
+    "revision": "3fd7c41e85fe20a69193",
+    "url": "/static/js/runtime-main.1a265c15.js"
+  }
+]);
